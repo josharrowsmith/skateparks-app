f6f0fd09b98f2d763d37825375a8497e243a8021
josh arrowsmith <josh.arrowsmith@hotmail.com>
2019-07-11 02:23:20 +1000

    developers cant design

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -2,7 +2,6 @@ import { createAppContainer, createStackNavigator } from "react-navigation";
 import Welcome from "../screens/Welcome";
 import HomeScreen from "../screens/HomeScreen"
 import SettingScreen from "../screens/Setting";
-// grab navigation
 
 const StackNavigator = createStackNavigator(
   {
