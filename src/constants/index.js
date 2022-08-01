37a4d4d58908fd50c0e7958161e9f89eececaf1f
Josh <josh.arrowsmith@hotmail.com>
2022-08-01 11:16:51 -0700

    checking out aws location for rebuild

--- src/constants/index.js
+++
@@ -1,6 +0,0 @@
-import device from "./device";
-import fonts from "./fonts";
-import func from "./functions";
-import globalStyles from "./globalStyles"
-
-export { device, fonts, func, globalStyles };
